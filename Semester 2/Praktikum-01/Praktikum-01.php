668a9555173c6845d5ae1d128a7ed02e7f94775c
NelanJoe <nelanjorelat@gmail.com>
2022-03-01 07:17:19 +0700

    PW - Semester 2

--- Semester 2/Praktikum-01/Praktikum-01.php
+++ Semester 2/Praktikum-01/Praktikum-01.php
@@ -15,33 +15,35 @@
 <body>
     <h1 class="text-center display-5 my-5">Daftar Nilai Mahasiswa</h1>
     <div class="container">
-        <table class="table table-hover">
-            <thead class="table-dark text-center">
-            <?php
-                $header = ['No', 'Nama', "NIM", 'UTS', 'UAS', 'Tugas', 'Nilai Akhir'];
-                foreach($header as $jdl) {
-                    echo "
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <table class="table table-hover">
+                    <thead class="table-dark text-center">
+                        <?php
+                        $header = ['No', 'Nama', "NIM", 'UTS', 'UAS', 'Tugas', 'Nilai Akhir'];
+                        foreach ($header as $jdl) {
+                            echo "
                         <th scope='col'>{$jdl}</th>
                     ";
-                }
-            ?>
-            </thead>
-            <tbody>
-                <?php
+                        }
+                        ?>
+                    </thead>
+                    <tbody>
+                        <?php
 
-                $ns_1 = ['id' => 1, 'nim' => '011022111', 'nama' => "Budi", 'uts' => 80, 'uas' => 84, 'tugas' => 90];
-                $ns_2 = ['id' => 1, 'nim' => '011023124', 'nama' => "Rudi", 'uts' => 60, 'uas' => 90, 'tugas' => 80];
-                $ns_3 = ['id' => 1, 'nim' => '011030311', 'nama' => "Riska", 'uts' => 70, 'uas' => 85, 'tugas' => 70];
-                $ns_4 = ['id' => 1, 'nim' => '011028313', 'nama' => "Siska", 'uts' => 50, 'uas' => 100, 'tugas' => 90];
+                        $ns_1 = ['id' => 1, 'nim' => '011022111', 'nama' => "Budi", 'uts' => 80, 'uas' => 84, 'tugas' => 90];
+                        $ns_2 = ['id' => 1, 'nim' => '011023124', 'nama' => "Rudi", 'uts' => 60, 'uas' => 90, 'tugas' => 80];
+                        $ns_3 = ['id' => 1, 'nim' => '011030311', 'nama' => "Riska", 'uts' => 70, 'uas' => 85, 'tugas' => 70];
+                        $ns_4 = ['id' => 1, 'nim' => '011028313', 'nama' => "Siska", 'uts' => 50, 'uas' => 100, 'tugas' => 90];
 
 
-                $ar_nilai = [$ns_1, $ns_2, $ns_3, $ns_4];
-                $nomor = 1;
-                foreach ($ar_nilai as $ns) {
-                    $nilai_akhir = ($ns['uts'] + $ns['uas'] + $ns['tugas']) / 3;
-                    $bg = $nomor % 2 == 1 ? 'table-primary' : 'table-secondary';
-                    echo "<tr class=' . $bg . text-center'>";
-                    echo '
+                        $ar_nilai = [$ns_1, $ns_2, $ns_3, $ns_4];
+                        $nomor = 1;
+                        foreach ($ar_nilai as $ns) {
+                            $nilai_akhir = ($ns['uts'] + $ns['uas'] + $ns['tugas']) / 3;
+                            $bg = $nomor % 2 == 1 ? 'table-primary' : 'table-secondary';
+                            echo "<tr class=' . $bg . text-center'>";
+                            echo '
                     <td>' . $nomor . '</td>
                     <td>' . $ns['nim'] . '</td>
                     <td>' . $ns['nama'] . '</td>
@@ -50,12 +52,14 @@
                     <td>' . $ns['tugas'] . '</td>
                     <td>' . number_format($nilai_akhir) . '</td>
                     ';
-                    echo "</tr>";
-                    $nomor++;
-                }
-                ?>
-            </tbody>
-        </table>
+                            echo "</tr>";
+                            $nomor++;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
     <!-- Bootstrap JS -->
